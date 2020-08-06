a754bc05dc144b167a1359110af55c4a4d648649
savvyguru <pungtuckwei@gmail.com>
2020-08-06 11:35:10 +0800

    second commit

--- dathena/quickstart/models.py
+++ dathena/quickstart/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from .validators import validate_file_extension
 
 # Create your models here.
 class File_Meta(models.Model):
     #owner = models.ForeignKey('auth.User', related_name='snippets', on_delete=models.CASCADE)
-    file = models.FileField(blank=False, null=False)
+    file = models.FileField(blank=False, null=False,validators=[validate_file_extension])
     score = models.IntegerField(null=True)
     timestamp = models.DateTimeField(auto_now=True)
